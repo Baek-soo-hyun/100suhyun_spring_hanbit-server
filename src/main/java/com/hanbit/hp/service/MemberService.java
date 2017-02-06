50293b3a69bfd0ea28e1117ce81f6af416f76c57
Baek-soo-hyun <100suhyun@naver.com>
2017-02-06 14:34:40 +0900

    2017.02.06 업데이트

--- src/main/java/com/hanbit/hp/service/MemberService.java
+++ src/main/java/com/hanbit/hp/service/MemberService.java
@@ -2,12 +2,17 @@ package com.hanbit.hp.service;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.hanbit.hp.dao.MemberDAO;
 
 @Service
 public class MemberService {
+	
+	private static final String SECRET_KEY = "hanbit1111";
+	private PasswordEncoder passwordEncoder = new StandardPasswordEncoder(SECRET_KEY);
 	
 	@Autowired
 	private MemberDAO memberDAO;
@@ -22,10 +27,24 @@ public class MemberService {
 		return key;
 	}
 	
+	public static void main(String[] args) {
+		PasswordEncoder passwordEncoder = new StandardPasswordEncoder("hanbit");
+		
+		String passwd1 = passwordEncoder.encode("1234");
+		
+		PasswordEncoder passwordEncoder2 = new StandardPasswordEncoder();
+		
+		String passwd2 = passwordEncoder.encode("1234");
+		
+		
+		System.out.println(passwordEncoder2.matches("1234", passwd2));
+	}
+	
 	public String addMember(String userId, String userPw) {
 		String uid = generateKey("UID");
+		String encryptedUserPw = passwordEncoder.encode(userPw);
 		
-		memberDAO.insertMember(uid, userId, userPw);
+		memberDAO.insertMember(uid, userId, encryptedUserPw);
 		
 		return uid;		
 	}	
