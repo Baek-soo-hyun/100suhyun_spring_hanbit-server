32b441ae72949d3d7c8178c870be00dad4d69e11
Baek-soo-hyun <100suhyun@naver.com>
2017-03-08 18:13:50 +0900

    2017.03.08 업데이트

--- src/main/resources/META-INF/resources/js/common.js
+++ src/main/resources/META-INF/resources/js/common.js
@@ -1,18 +1,19 @@
 define([
-    "bootstrap",
+	"bootstrap",
 ], function() {
 	$(".admin-logo").on("click", function() {
-		location.href = "/admin";
+		location.href = "/admin/";
 	});
 	
 	$(".admin-menu>ul>li").on("click", function() {
 		var menu = $(this).attr("menu");
+		
 		location.href = "/admin/" + menu;
 	});
 	
 	if (_ctx.menuId && _ctx.menuId !== "") {
 		$(".admin-menu>ul>li[menu='" + _ctx.menuId + "']")
-		.addClass("active");	
+			.addClass("active");
 	}
 	
 	function showSection(section, jqElement, handler) {
@@ -21,22 +22,19 @@ define([
 		
 		handler(section, jqElement);
 	}
-	
+
 	function initMgmt(handler) {
 		$(".btn-admin-add").on("click", function() {
-			showSection(".admin-add", $(this), handler);
+			showSection(".admin-add", null, handler);
 		});
 		
 		$(".btn-admin-cancel").on("click", function() {
 			showSection(".admin-list", null, handler);
 		});
 		
-		$(".admin-list table>tbody>tr").on("click", function() {
-			showSection(".admin-update", $(this), handler);
-		});	
-		
 		showSection(".admin-list", null, handler);
 	}
+	
 	
 	// 밖에서도 부를 수 있게
 	return {
